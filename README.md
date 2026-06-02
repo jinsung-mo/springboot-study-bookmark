c856300a503d7f1dd43eb05d3723fc16ddede056
jinsung-mo <ahwlstjd57@gmail.com>
2026-06-02 09:42:29 +0900

    Initial commit of hanbit project

---
+++ README.md
@@ -0,0 +1,9 @@
+# hanbit
+
+This is a Spring Boot project.
+
+## Build and Run
+
+```bash
+./mvnw spring-boot:run
+```
---
+++ src/main/java/kr/co/hanbit/Bookmark/ServerErrorRestController.java
@@ -0,0 +1,13 @@
+package kr.co.hanbit.Bookmark;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServerErrorRestController {
+
+    @RequestMapping("/throwServerError")
+    public void throwServerError(){
+        throw new RuntimeException();
+    }
+}
